e6fcddd8265bede0d06439630894c4f5f3d719ba
Nenad Jovanovik <nenad.jovanovik@sirma.bg>
2023-10-07 14:29:25 +0200

    zcxcxz

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,6 +7,9 @@ import React, { useState } from "react";
 function Expenses(props) {
   const [filteredYear, setFIlteredYear] = useState("2020");
   let filterInfoText = "2019,2021 & 2022";
+  const filteredItems = props.items.filter(item => {
+    return item.date.getFullYear().toString() === filteredYear;
+  });
 
   if (filteredYear === "2019") {
     filterInfoText = "2020, 2021 & 2022";
@@ -28,29 +31,14 @@ function Expenses(props) {
           onChangeFilter={filterChangeHandler}
         />
         <p>Data for years {filterInfoText} is hidden</p>
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        ></ExpenseItem>
-
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        ></ExpenseItem>
-
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        ></ExpenseItem>
-
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        ></ExpenseItem>
+        {filteredItems.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
